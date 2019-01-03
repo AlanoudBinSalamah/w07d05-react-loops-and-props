81e0c24a7d6214325ab899fa97174f65a04e3085
Jackie Casper <casper.jaclyn@gmail.com>
2019-01-03 10:46:30 +0300

    add blog

---
+++ bob-blobs-bog-blog/src/data/blogPosts.js
@@ -0,0 +1,52 @@
+const blogPosts = [
+  {
+    title: "Are Spoons Actually Alien Artifcacts?",
+    author: "Jackie",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid voluptates odit consequatur voluptatibus nesciunt nobis. Consequatur porro atque aspernatur vero, tempora odio iure adipisci, nobis, saepe dolor assumenda aliquid.",
+    comments: [
+      "intriguing!", "is that why I can't use them upside down?", "does this look infected to you?"
+    ]
+  },
+  {
+    title: "I read Lord of the Rings Backwards and This is What I Learned",
+    author: "Trevor",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quod aliquid similique velit consequatur recusandae ea! Modi deleniti esse similique veritatis rerum, sint recusandae consectetur repudiandae? Nulla iure porro blanditiis.",
+    comments: [
+      "by Dr. Suess, right?", "Darth Vader was a great character", "I think this is neat", "wow avocado"
+    ]
+  },
+  {
+    title: "Origins of Oranges",
+    author: "Jackie",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quod aliquid similique velit consequatur recusandae ea! Modi deleniti esse similique veritatis rerum, sint recusandae consectetur repudiandae? Nulla iure porro blanditiis.",
+    comments: [
+      "why can't fruit be compared?", "I adopted and orange", "this is a hoax", "I prefer mangos"
+    ]
+  },
+  {
+    title: "Couches are Just Really Long Chairs",
+    author: "Trevor",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quod aliquid similique velit consequatur recusandae ea! Modi deleniti esse similique veritatis rerum, sint recusandae consectetur repudiandae? Nulla iure porro blanditiis.",
+    comments: [
+      "or are chairs really short couches?", "I don't believe in couches", "I always get the spellings for 'couch' and 'coach' confused", "booooo"
+    ]
+  },
+  {
+    title: "I Don't Believe in Crayons",
+    author: "Jackie",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quod aliquid similique velit consequatur recusandae ea! Modi deleniti esse similique veritatis rerum, sint recusandae consectetur repudiandae? Nulla iure porro blanditiis.",
+    comments: [
+      "I liek to eat crayons", "I read your expose on oranges, you are amazing!", "I think you should spell your name like 'Jacki'"
+    ]
+  },
+  {
+    title: "What if We Lived In A World Where Cats Didn't Have Legs and Just Hovered Around?",
+    author: "Ghadeer",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quod aliquid similique velit consequatur recusandae ea! Modi deleniti esse similique veritatis rerum, sint recusandae consectetur repudiandae? Nulla iure porro blanditiis.",
+    comments: [
+      "My cat can hover for short amounts of time", "What about dogs?", "meow I iz a cat and I luv dis"
+    ]
+  },
+] 
+
+export default blogPosts;
